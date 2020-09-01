17605bacb3298da520f24a963118a6c32a960e9d
Ubuntu <ubuntu@ip-172-31-71-223.ec2.internal>
2020-09-01 03:31:15 +0000

    1

---
+++ client/build/precache-manifest.a4e70ba3aae368d72a34a33965181bc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae4805dcde473ea4c6fb",
+    "url": "/static/css/main.ef211277.chunk.css"
+  },
+  {
+    "revision": "ae4805dcde473ea4c6fb",
+    "url": "/static/js/main.f4266d5c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c39f927c3d5be3c1ffe8",
+    "url": "/static/js/2.fc7915e9.chunk.js"
+  },
+  {
+    "revision": "78cbf04ec01071373c3990d201f9cfee",
+    "url": "/static/media/acorn-green-nofill-text.78cbf04e.svg"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/static/media/Quicksand-VariableFont_wght.f9baef8a.ttf"
+  },
+  {
+    "revision": "9c56c992c12f9b868fa0741d08b7381a",
+    "url": "/index.html"
+  }
+];
